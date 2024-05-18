4c11b8b7a3ffa4f52475c8f4f21d861243c6cb3c
kutegz <wilson.kutegeka@clinicmaster.net>
2024-05-18 11:20:33 +0300

    Minor Changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
-  return {
-    server: { port: 3000, open: true },
-    plugins: [react()],
-  };
+  return { plugins: [react()] };
 });
